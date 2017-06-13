e72636b0cea04d2a77cbb100d254cc6709d0b80d
david gray <davidseangray@gmail.com>
2017-06-13 14:33:39 +0100

    responds with 200

---
+++ expressServer.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var cache = {};
+
+app.get('/', function (req, res) {
+  res.send('Hello world!')
+});
+
+app.listen(4000, function () {
+  console.log('listening on port 4000')
+});
+
+app.post('/set', function(req, res) {
+  var query = req.query;
+  Object.keys(query).forEach(function(key) {
+    cache[key] = query[key];
+  });
+  res.status(200).end();
+});
+
+
+module.exports = app
